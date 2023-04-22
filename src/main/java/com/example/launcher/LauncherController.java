2d3976a20b5ab8e342f9074ea3e11e8c61cdd73e
dmitry.glazunov <dmitry.glazunov@red-soft.ru>
2023-04-22 19:35:25 +0300

    Log label is added for dev needs. It is available to start the Terraria from launcher.

--- src/main/java/com/example/launcher/LauncherController.java
+++ src/main/java/com/example/launcher/LauncherController.java
@@ -1,9 +1,14 @@
 package com.example.launcher;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.io.File;
+import java.io.IOException;
 
 public class LauncherController {
+    @FXML
+    private Label log;
     @FXML
     private TextField pathToWorld;
     @FXML
@@ -17,6 +22,40 @@ public class LauncherController {
     }
     @FXML
     protected void onButtonLaunchGame() {
+        if (checkTextFields()) {
+            String path_to_game_folder = pathToGame.getText();
+            File game_directory = new File(path_to_game_folder);
+            if (game_directory.exists() && game_directory.isDirectory()) {
+                ProcessBuilder pb = null;
+                switch (System.getProperty("os.name")) {
+                    case "Linux": {
+                        pb = new ProcessBuilder(String.format("%s/Terraria", path_to_game_folder));
+                        break;
+                    }
+                    case "Windows": {
+                        pb = new ProcessBuilder(String.format("%s/Terraria.exe", path_to_game_folder));
+                        break;
+                    }
+                    default: {
+                        log.setText("Не знаю такой опреационной ситсемы!");
+                    }
+                }
+                if (pb != null) {
+                    try {
+                        Process proc = pb.start();
+                    } catch (IOException e) {
+                        log.setText(e.getMessage());
+                    }
+                } else {
+                    log.setText("Не удалось построить процесс!");
+                }
+            } else {
+                log.setText("Папка не существует!");
+            }
+        }
+    }
 
+    private boolean checkTextFields() {
+        return !pathToWorld.getText().isEmpty() && !pathToGame.getText().isEmpty() && !pathToCloud.getText().isEmpty();
     }
 }
