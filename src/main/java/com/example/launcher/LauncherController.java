89e5737f3e4ececc0faf4cc86636132f7aaa3cfc
dmitry.glazunov <dmitry.glazunov@red-soft.ru>
2023-04-28 23:56:53 +0300

    Upload file is developed.

--- src/main/java/com/example/launcher/LauncherController.java
+++ src/main/java/com/example/launcher/LauncherController.java
@@ -1,11 +1,16 @@
 package com.example.launcher;
 
+import com.google.gson.reflect.TypeToken;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import com.google.gson.Gson;
 
 public class LauncherController {
@@ -17,23 +22,59 @@ public class LauncherController {
     private TextField token;
     @FXML
     private TextField pathToGame;
+    @FXML
+    public TextField nickName;
     private Process game = null;
     private final String link_to_download = "https://cloud-api.yandex.net/v1/disk/resources/download";
     private final String link_to_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload";
     private final Gson parser = new Gson();
+    private Type listOfHostsType = new TypeToken<HashMap<Integer, HostInfo>>(){}.getType();
+    private Integer current_host_number = -1;
     @FXML
     protected void onButtonBecomeHost() {
         if (checkTextFields()) {
             if (game == null) {
                 // Скачать hosts.json
                 URL downloadLink = this.getLink(this.link_to_download, "hosts.json");
-                if (downloadFile(" ./hosts.json", 1024, downloadLink)) {
-                    log.setText("SUCCESS!");
-                } else {
-                    log.setText("Error: can not download the \"hosts.json\" file.");
-                }
+                if (downloadFile("hosts.json", 1024, downloadLink)) {
+                    try {
+                        Reader hosts_reader = new FileReader("hosts.json");
+                        HashMap<Integer, HostInfo> data = parser.fromJson(hosts_reader, this.listOfHostsType);
+                        for (Map.Entry<Integer, HostInfo> it : data.entrySet()) {
+                            if (this.nickName.getText().equals(it.getValue().nick_name)) {
+                                this.current_host_number = it.getKey();
+                                break;
+                            }
+                        }
+                        hosts_reader.close();
+                    } catch (IOException e) {
+                        log.setText(e.getMessage());
+                    }
+                    if (this.current_host_number == -1) {
+                        log.setText("Указанный никнейм не существует.");
+                    } else {
+                        log.setText("Ваш id - " + this.current_host_number.toString());
+                    }
 
-                // Загрузить файл
+                    // Загрузить <номер>.json
+                    String file_name = this.current_host_number.toString() + ".json";
+                    try {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(file_name));
+                        Date date_now = new Date();
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+                        TimestampInfo tmst = new TimestampInfo(sdf.format(date_now));
+                        writer.write(this.parser.toJson(tmst));
+                        writer.close();
+                    } catch (IOException e) {
+                        log.setText(e.getMessage());
+                    }
+                    URL uploadLink = this.getLink(this.link_to_upload, file_name);
+                    if (this.uploadFile(file_name, 1024, uploadLink)) {
+                        log.setText("SUCCESS!");
+                    } else {
+                        log.setText("Failed to upload!");
+                    }
+                }
             }
         }
     }
@@ -73,7 +114,7 @@ public class LauncherController {
     }
 
     private boolean checkTextFields() {
-        return !pathToWorld.getText().isEmpty() && !pathToGame.getText().isEmpty() && !token.getText().isEmpty();
+        return !pathToWorld.getText().isEmpty() && !pathToGame.getText().isEmpty() && !token.getText().isEmpty() && !nickName.getText().isEmpty();
     }
 
     private URL getLink(String base_link, String file) {
@@ -118,6 +159,28 @@ public class LauncherController {
             }
             in.close();
             file_hosts.close();
+        } catch (IOException e) {
+            log.setText(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    private boolean uploadFile(String file_path, int buffer_size, URL link) {
+        try {
+            HttpURLConnection con = (HttpURLConnection) link.openConnection();
+            con.setRequestMethod("PUT");
+            con.setDoOutput(true);
+
+            BufferedOutputStream out = new BufferedOutputStream(con.getOutputStream());
+            BufferedInputStream number_file = new BufferedInputStream(new BufferedInputStream(new FileInputStream(file_path)));
+            byte[] buffer = new byte[buffer_size];
+            int bytes_read;
+            while ((bytes_read = number_file.read(buffer, 0, buffer_size)) != -1) {
+                out.write(buffer, 0, bytes_read);
+            }
+            number_file.close();
+            out.close();
+            con.disconnect();
         } catch (IOException e) {
             log.setText(e.getMessage());
             return false;
