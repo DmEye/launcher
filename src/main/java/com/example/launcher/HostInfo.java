0c6b3dada208b1cdad567a820e0b0022ae69741a
dmitry.glazunov <dmitry.glazunov@red-soft.ru>
2023-04-28 23:57:46 +0300

    Added forgotten files

---
+++ src/main/java/com/example/launcher/HostInfo.java
@@ -0,0 +1,12 @@
+package com.example.launcher;
+
+public class HostInfo {
+    public String nick_name;
+    public HostInfo(String nick_name) {
+        this.nick_name = nick_name;
+    }
+
+    public String toString() {
+        return nick_name;
+    }
+}
---
+++ src/main/java/com/example/launcher/TimestampInfo.java
@@ -0,0 +1,8 @@
+package com.example.launcher;
+
+public class TimestampInfo {
+    public String timestamp;
+    public TimestampInfo(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
